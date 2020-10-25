944a8cc0a43c734888bf686b9502115826d47158
Daniil Demchenko <danyadw2000@gmail.com>
2020-10-25 17:17:16 +0300

    add fields

--- src/main/java/entity/BaseContract.java
+++ src/main/java/entity/BaseContract.java
@@ -1,4 +1,32 @@
 package entity;
 
+import java.util.Calendar;
+
 public class BaseContract {
+    int id;
+    Calendar start, end;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Calendar getStart() {
+        return start;
+    }
+
+    public void setStart(Calendar start) {
+        this.start = start;
+    }
+
+    public Calendar getEnd() {
+        return end;
+    }
+
+    public void setEnd(Calendar end) {
+        this.end = end;
+    }
 }
--- src/main/java/entity/Client.java
+++ src/main/java/entity/Client.java
@@ -1,4 +1,49 @@
 package entity;
 
+import java.util.Calendar;
+
 public class Client {
+    int id, passport;
+    String FullName, gender;
+    Calendar DOB;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPassport() {
+        return passport;
+    }
+
+    public void setPassport(int passport) {
+        this.passport = passport;
+    }
+
+    public String getFullName() {
+        return FullName;
+    }
+
+    public void setFullName(String fullName) {
+        FullName = fullName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Calendar getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(Calendar DOB) {
+        this.DOB = DOB;
+    }
 }
